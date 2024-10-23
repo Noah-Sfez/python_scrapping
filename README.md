f09ba44001c4c5b170427d0349b860eb03f44106
Noah Sfez <sfz.noah@gmail.com>
2024-10-23 10:30:26 +0200

    Phase 2

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ PHASE 1 :
 Récupération de l'url de la page d'un livre sur la page d'accueil
 Une fois la l'url dédiée, je récupère toutes les informations nécessaires sur le livre
 J'ajoute toutes les informations dans un fichier csv : book.csv
+
+PHASE 2 : 
+Modification du code pour en faisant des fonctions pour éviter de réécrire plusieurs les mêmes lignes
+Je récupère dedans l'url d'une catégorie, et une fois l'url stockée, je scrappe les données de tous les livres de la catégorie
+Si il y'a plusieurs pages à l'intérieur de la catégorie, je passe à la page suivante, une fois tous les livres de la page scrappés
--- app.py
+++ app.py
@@ -1,42 +1,103 @@
-from bs4 import BeautifulSoup
 import csv
 import requests
-    
+from bs4 import BeautifulSoup
 
-with open('book.csv', 'w', newline='', encoding='utf8') as fichier_csv:
+def get_book_details(url, soup_book):
     """
-    Ici on va récupérer les informations des livres de la page d'accueil du site
-    D'abord je récupère les liens des livres de la page d'accueil
-    Ensuite je récupère les informations des livres sur leur page respective
-    Et enfin j'écris les informations dans un fichier csv
+    Ici on recupère les détails du livre et on les retourne sous forme de liste
     """
+    try:
+        title = soup_book.find('h1').text
+        upc = soup_book.find_all('td')[0].text
+        price_including_tax = soup_book.find_all('td')[3].text.replace('Â', '')
+        price_excluding_tax = soup_book.find_all('td')[2].text.replace('Â', '')
+        number_available = soup_book.find_all('td')[5].text.replace('In stock (', '').replace(' available)', '')
+        review_rating = soup_book.find_all('p', class_='star-rating')[0]['class'][1]
+        image_url = soup_book.find('img')['src'].replace('../../', '')
+        img_url = url + image_url
+        category = soup_book.find_all('ul', class_='breadcrumb')[0].find_all('a')[2].text
+        div_description = soup_book.find('div', id='product_description')
+        description = div_description.find_next('p').text if div_description else "No description available"
+        return [title, upc, price_including_tax, price_excluding_tax, number_available, description, review_rating, img_url, category]
+    except Exception as e:
+        print(f"Erreur lors de la récupération des détails du livre : {e}")
+        return None
 
-    writter = csv.writer(fichier_csv)
+def process_books_on_page(soup_category, url, writer, number_books):
+    """
+    Ici on traite les livres sur une page et on les écrit dans le fichier csv
+    """
+    for i in range(0, number_books + 1):
+        try:
+            link = soup_category.find_all('h3')[i].find('a')['href']
+            url_book = url + 'catalogue/' + link.replace('../', '')
+            print(f"Processing book URL: {url_book}")
+            response_book = requests.get(url_book)
+            soup_book = BeautifulSoup(response_book.text, 'html.parser')
+            book_details = get_book_details(url, soup_book)
+            if book_details:
+                writer.writerow([url_book] + book_details)
+        except IndexError:
+            print(f"No more books on this page at index {i}.")
+            break
+        except Exception as e:
+            print(f"Erreur lors du traitement du livre : {e}")
+            continue
+
+def process_next_page(soup_category, url_category, url, writer):
+    """
+    Ici on va traier la page suivante pour plus tard récupérer les livres de cette page
+    """
+    try:
+        next_page = soup_category.find('li', class_='next').find('a')['href']
+        url_next_page = url_category.replace('index.html', '') + next_page
+        response_category = requests.get(url_next_page)
+        soup_category_next = BeautifulSoup(response_category.text, 'html.parser')
+        number_books = int(soup_category_next.find('form', class_='form-horizontal').find('strong').text)
+        process_books_on_page(soup_category_next, url, writer, number_books)
+            
+    except Exception as e:
+        print(f"Erreur lors de la récupération de la page suivante : {e}")
+
+def scraping_books(url_category, url, writer):
+    """
+    Ici on va scraper les livres de la catégorie et les écrire dans le fichier csv
+    Quand on a fini de traiter une page, on va chercher la page suivante
+    Si on ne trouve plus de boutons Next sur la page, on arrête le scraping
+    """
+    try:
+        response_category = requests.get(url_category)
+        soup_category = BeautifulSoup(response_category.text, 'html.parser')
+        number_books = int(soup_category.find('form', class_='form-horizontal').find('strong').text)
+        process_books_on_page(soup_category, url, writer, number_books)
+
+        while soup_category.find('li', class_='next'):
+            process_next_page(soup_category, url_category, url, writer)
+            next_page = soup_category.find('li', class_='next').find('a')['href']
+            url_next_page = url_category.replace('index.html', '') + next_page
+            response_category = requests.get(url_next_page)
+            soup_category = BeautifulSoup(response_category.text, 'html.parser')
+            print(soup_category.find('li', class_='next'))
+            if not soup_category.find('li', class_='next'):
+                print("No more pages to process.")
+                break
+    except Exception as e: 
+        print(f"Erreur générale lors du scraping : {e}")
+
+if __name__ == "__main__":
+    """
+    On fait tourner tout le programme, et on va mettre les en tête du fichier csv  et appeler la fonction scraping_books
+    """
     url = 'https://books.toscrape.com/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    books = soup.find_all('div', class_='image_container')
-    link = books[0].find('a')['href']
-    url_book = url + link
-    response_book = requests.get(url_book)
-    soup_book = BeautifulSoup(response_book.text, 'html.parser')
 
-    # Récupération des informations des livres
-    title = soup_book.find('h1').text
-    upc = soup_book.find_all('td')[0].text
-    price_including_tax = soup_book.find_all('td')[3].text.replace('Â', '')
-    price_excluding_tax = soup_book.find_all('td')[2].text.replace('Â', '')
-    number_available = soup_book.find_all('td')[5].text
-    review_rating = soup_book.find_all('td')[6].text
-    image_url = soup_book.find('img')['src']
-    category = soup_book.find_all('ul', class_='breadcrumb')[0].find_all('a')[2].text
-    # Récupération de la description
-    div_description = soup_book.find('div', id='product_description')
-    description = div_description.find_next('p').text
-    # J'écris les informations dans le csv
-    writter.writerow(['product_page_url','title', 'upc', 'prince_including_tax', 'prince_excluding_tax', 'number_available', 'product_description', 'review_rating', 'image_url', 'category'])
-    writter.writerow([ url_book, title, upc, price_including_tax, price_excluding_tax, number_available, description, review_rating, image_url, category])
-    
-
-
-
+    with open('category.csv', 'w', newline='', encoding='utf8') as fichier_csv:
+        writer = csv.writer(fichier_csv)
+        writer.writerow(['product_page_url', 'title', 'upc', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'review_rating', 'image_url', 'category'])
+        
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        categories = soup.find_all('ul', class_='nav nav-list')
+        link = categories[0].find_all('a')[4]['href']  
+        url_category = url + link
+        
+        scraping_books(url_category, url, writer)
